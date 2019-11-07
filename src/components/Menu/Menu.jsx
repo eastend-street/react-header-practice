78d55b476d1a6903cd4e2c19faa42e0f93ee2b1f
Jun Yamada <43656115+eastend-street@users.noreply.github.com>
2019-11-06 19:22:24 -0800

    Added change menu function

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -6,33 +6,58 @@ class Menu extends React.Component {
   state = {
     navLinkArr: [
       {
+        id: 1,
         text: "Overview",
-        url: "",
+        url: "#overview",
         isCurrentPage: true
       },
       {
+        id: 2,
         text: "The core",
-        url: "",
+        url: "#core",
         isCurrentPage: false
       },
       {
+        id: 3,
         text: "Solutions",
-        url: "",
+        url: "#solutions",
         isCurrentPage: false
       },
       {
+        id: 4,
         text: "About",
-        url: "",
+        url: "#about",
         isCurrentPage: false
       }
     ]
   };
 
+  handleClick = id => {
+    const newArr = this.state.navLinkArr.map(eachNav => {
+      if (eachNav.id === id) {
+        eachNav.isCurrentPage = true;
+      } else {
+        eachNav.isCurrentPage = false;
+      }
+      return eachNav;
+    });
+    this.setState({ navLinkArr: newArr });
+  };
+
   render() {
     return (
       <div className="menu">
-        {this.state.navLinkArr.map(eachNav => {
-          return <NavLink text={eachNav.text} />;
+        {this.state.navLinkArr.map((eachNav, index) => {
+          return (
+            <NavLink
+              key={index}
+              text={eachNav.text}
+              url={eachNav.url}
+              isCurrentPage={eachNav.isCurrentPage}
+              parentFunc={this.handleClick}
+              id={eachNav.id}
+            />
+          );
         })}
         <div className="button">Let's start</div>
       </div>
--- src/components/NavLink/NavLink.jsx
+++ src/components/NavLink/NavLink.jsx
@@ -1,12 +1,27 @@
 import React from "react";
 import "./NavLink.scss";
 
-const NavLink = ({ text, url, isCurrentPage }) => {
-  return (
-    <div className="nav-link">
-      <a href={url}>{text}</a>
-    </div>
-  );
-};
+class NavLink extends React.Component {
+  navStyle = () => {
+    let baseClass = ["nav-link"];
+    if (this.props.isCurrentPage) {
+      baseClass.push("active");
+    }
+    return baseClass.join(" ");
+  };
+
+  handleClick = e => {
+    e.preventDefault();
+    this.props.parentFunc(this.props.id);
+  };
+
+  render() {
+    return (
+      <div className={this.navStyle()}>
+        <a onClick={this.handleClick}>{this.props.text}</a>
+      </div>
+    );
+  }
+}
 
 export default NavLink;
